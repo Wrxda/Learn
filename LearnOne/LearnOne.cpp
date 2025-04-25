3ff566bf9e9c551b6ff4c70686b133aa75daf90a
Vasilii <Vasilii0600@mail.ru>
2025-04-25 16:01:57 +0600

    LearnOne

---
+++ LearnOne/LearnOne.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+int main() {
+	using namespace std;
+	setlocale(LC_ALL, "RU");
+
+	int a, b, c;
+
+	cout << "Введи свой возраст\n";
+	cin >> a;
+	cout << "Ваш возраст: " << a << endl;
+	if (a < 18) {
+		cout << "Ты ещё школьник!\n";
+	}
+	else {
+		cout << "Ты взрослый человек!\n";
+	}
+}

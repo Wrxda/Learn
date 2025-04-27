3a43bac4acbff37ea5a83a8abd573e1c65a3bb6a
Vasilii <Vasilii0600@mail.ru>
2025-04-27 13:37:14 +0600

    passed tasks seven

---
+++ Tasks/TasksSeven/TasksSeven.cpp
@@ -0,0 +1,28 @@
+﻿//1. Распространённая задача:  Дано четырехзначное число (к примеру 5678), 
+// вывести на экран  в обратном порядке цифры из которых это число состоит. 
+// То есть мы должны увидеть на экране 8765.
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	int number = 9832;
+	cout << "Дано число: " << number<<endl;
+	cout << "Число наизнанку: ";
+
+	cout << number % 10;
+	number /= 10;
+	cout << number % 10;
+	number /= 10;
+	cout << number % 10;
+	number /= 10;
+	cout << number % 10;
+	number /= 10;
+
+	
+	return 0;
+}
